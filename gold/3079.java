2c7398683ab7ee65c9ecab5731bac3950b9f0d20
Harin Kim <62981406+rineeee@users.noreply.github.com>
2024-04-01 21:43:37 +0900

    Create 3079.java

---
+++ gold/3079.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static long m, max;
+    static int [] arr;
+    static long result = Long.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max,arr[i]);
+        }
+        Arrays.sort(arr);
+
+        solve();
+
+        System.out.println(result);
+    }
+
+    private static void solve(){
+        long low = 0;
+        long high = m * max;
+
+        while(low<=high){
+            long mid = (low+high)/2;
+            long sum = 0;
+            for(long index: arr){
+                long count = mid/index;
+
+                if(sum>=m){
+                    break;
+                }
+                sum+=count;
+            }
+            if(sum>=m){
+                high = mid-1;
+                result = Math.min(mid,result);
+            }
+            else{
+                low = mid+1;
+            }
+        }
+    }
+}
